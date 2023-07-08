4996ea250b412834ae9c52ccf261c8d0b6b582bc
Mateus Oliveira <75847514+m0liveira@users.noreply.github.com>
2023-07-08 03:06:39 +0100

    feat: 🎸 form validations
    added validations to the login form

--- WishHub-Extension/src/app/components/auth/login/login.component.ts
+++ WishHub-Extension/src/app/components/auth/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 import { Router } from '@angular/router';
 import { FirebaseService } from "../../../services/firebase.service";
 
@@ -16,30 +16,103 @@ export class LoginComponent implements OnInit {
   isMailwritten: boolean = false;
   isPasswordFocused: boolean = false;
   isPasswordwritten: boolean = false;
+  errors: Array<string> = [];
 
   constructor(public firebaseService: FirebaseService, private router: Router) { }
 
   ngOnInit(): void {
     this.form = new FormGroup({
-      'email': new FormControl(),
-      'password': new FormControl(),
+      'email': new FormControl('', [Validators.email, this.noSpaceAllowed, this.noEmptyAllowed]),
+      'password': new FormControl('', [this.lengthRangeAllowed, this.noSpaceAllowed, this.noEmptyAllowed]),
     });
   }
 
   logIn() {
-    // this.firebaseService.AddUserToDatabase('GHaFtnRL3NiUNVB2', 'andré', 'email@123.com', 'noavatar.png');
+    this.errors = [];
+
+    Object.values(this.form.controls).forEach(control => {
+      control.markAsTouched();
+    });
+
+    if (this.form.valid === false) {
+      Object.keys(this.form.controls).forEach(key => {
+        let control: any = this.form.controls[key];
+
+        if (control.errors) {
+          Object.keys(control.errors).forEach(error => {
+            this.errors.push(control.errors[error])
+          });
+        };
+      });
+
+      console.log(this.errors);
+      return;
+    }
 
     console.log(this.form.value);
-    alert('carregado');
+
+    // this.firebaseService.AddUserToDatabase('GHaFtnRL3NiUNVB2', 'andré', 'email@123.com', 'noavatar.png');
   }
 
-  togglePassword() {
+  togglePassword(input: HTMLInputElement) {
     this.isVisible = !this.isVisible;
+    this.isVisible ? input.type = 'text' : input.type = 'password';
     this.isPasswordFocused = true;
   }
 
   onInput(): void {
     this.form.value.email !== '' && this.form.value.email !== null ? this.isMailwritten = true : this.isMailwritten = false;
     this.form.value.password !== '' && this.form.value.password !== null ? this.isPasswordwritten = true : this.isPasswordwritten = false;
+  }
+
+  // Form validations
+
+  noSpaceAllowed(control: AbstractControl) {
+    let parentControl: any = control.parent;
+    let controlName;
+
+    if (parentControl) {
+      controlName = Object.keys(parentControl.controls).find(key => parentControl.controls[key] === control);
+    }
+
+    if (control.value !== null && control.value.indexOf(' ') !== -1) {
+      return { noSpaceAllowed: `Your ${controlName} can't contain spaces.` };
+    }
+
+    return null;
+  }
+
+  noEmptyAllowed(control: AbstractControl) {
+    let parentControl: any = control.parent;
+    let controlName;
+
+    if (parentControl) {
+      controlName = Object.keys(parentControl.controls).find(key => parentControl.controls[key] === control);
+    }
+
+    if (control.value !== null && control.value === '') {
+      return { error: `Your ${controlName} can't be empty.` };
+    }
+
+    return null;
+  }
+
+  lengthRangeAllowed(control: AbstractControl) {
+    let parentControl: any = control.parent;
+    let controlName;
+
+    if (parentControl) {
+      controlName = Object.keys(parentControl.controls).find(key => parentControl.controls[key] === control);
+    }
+
+    if (control.value !== null && control.value.length < 6) {
+      return { error: `Your ${controlName} must contain a minimum of 6 characters.` };
+    }
+
+    if (control.value !== null && control.value.length > 24) {
+      return { error: `Your ${controlName} must have less then 24 characters.` };
+    }
+
+    return null;
   }
 }
--- WishHub-Extension/src/app/services/firebase.service.ts
+++ WishHub-Extension/src/app/services/firebase.service.ts
@@ -1,22 +1,22 @@
 import { Injectable } from '@angular/core';
-// import { environment } from 'src/environments/myEnv';
-// import { initializeApp } from "firebase/app";
-// import { getDatabase, ref, set } from "firebase/database";
+import { environment } from 'src/environments/environment';
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, set } from 'firebase/database';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class FirebaseService {
-  // app = initializeApp(environment.firebaseConfig);
+  app = initializeApp(environment.firebaseConfig);
 
   constructor() { }
 
   // prototype
-  // AddUserToDatabase(userId: string, name: string, email: string, imageUrl: string) {
-  //   let db = getDatabase();
-  //   let reference = ref(db, 'users/' + userId);
+  AddUserToDatabase(userId: string, name: string, email: string, imageUrl: string) {
+    let db = getDatabase();
+    let reference = ref(db, 'users/' + userId);
 
-  //   set(reference, { username: name, email: email, avatar: imageUrl });
-  // };
+    set(reference, { username: name, email: email, avatar: imageUrl });
+  };
 }
