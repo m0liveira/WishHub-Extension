c2bb12ec55edb6af38221e20527b6bacc50857d6
Mateus Oliveira <75847514+m0liveira@users.noreply.github.com>
2023-07-23 21:30:52 +0100

    style: 💄 Grid UI
    added a functionality to change from list view to grid view + changed
    the UI to suit that setting

--- WishHub-Extension/src/app/components/hub/hub.component.ts
+++ WishHub-Extension/src/app/components/hub/hub.component.ts
@@ -6,12 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hub.component.scss']
 })
 export class HubComponent implements OnInit {
+  isGrid: boolean = true;
   lists: Array<any> = [
     { photo: '../../../assets/Placeholder.png', group: false, name: 'birthday', items: 3, views: 0 },
     { photo: '../../../assets/Placeholder.png', group: false, name: 'christmas wishlist', items: 10, views: 3 },
     { photo: '../../../assets/Placeholder.png', group: true, name: 'random things', items: 35, views: 457 },
     { photo: '../../../assets/Placeholder.png', group: false, name: 'i just want this', items: 125, views: 72 }
-  ]
+  ];
 
   constructor() { }
 
